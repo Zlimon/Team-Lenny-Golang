2e4589c332c1c88df806358da30e2a6a5eaa2eb5
Magnus Husveg <magnh17@uia.no>
2018-01-26 11:40:03 +0100

    Magnu

--- HelloWorld.go
+++ HelloWorld.go
@@ -5,7 +5,7 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, Petter")
 	fmt.Println("Hello, Simon")
-	fmt.Println("Hello, Magnus")
+	fmt.Println("Hello, Magnu")
 	fmt.Println("Hello, Kristian")
 	fmt.Println("Hello, Marius")
 }

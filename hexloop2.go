afe79b581144316c7cdb16ad5903b88db9d8002f
Petter Sølvsberg Johannessen <35605803+petterjohannessen@users.noreply.github.com>
2018-02-14 17:22:21 +0100

    Add files via upload
    Oppgave 4

---
+++ hexloop2.go
@@ -0,0 +1,14 @@
+package main
+
+import(
+	"fmt"
+
+)
+func main() {
+	for j := 0x80; j <= 0xFF; j++ {
+		fmt.Printf("%X  ", j)
+		fmt.Printf("%c  ", j)
+		fmt.Printf("%b  ", j)
+		fmt.Println("")
+	}
+}

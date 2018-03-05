5b53e6361a0931f64a1a0b4ae52117cca07e479d
Petter Sølvsberg Johannessen <35605803+petterjohannessen@users.noreply.github.com>
2018-03-05 14:07:14 +0100

    Add files via upload
    oppgave 1

--- obligatorisk-oppgave-2/src/oppgave1/fileinfo.go
+++ obligatorisk-oppgave-2/src/oppgave1/fileinfo.go
@@ -1,1 +1,29 @@
+package main
 
+import (
+	"fmt"
+	"log"
+	"os"
+	"bufio"
+)
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Enter Filename: ")
+	text, _ := reader.ReadString('\n')
+	fmt.Println(text)
+	fi, err := os.Lstat("")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	switch mode := fi.Mode(); {
+	case mode.IsRegular():
+		fmt.Println("regular file")
+	case mode.IsDir():
+		fmt.Println("directory")
+	case mode&os.ModeSymlink != 0:
+		fmt.Println("symbolic link")
+
+	}
+}

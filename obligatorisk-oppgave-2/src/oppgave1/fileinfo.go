e7778449f2d7e1c688833b4201eb9b7855063e4a
Petter Sølvsberg Johannessen <pettej17@uia.no>
2018-03-05 14:30:25 +0100

    +append

--- obligatorisk-oppgave-2/src/oppgave1/fileinfo.go
+++ obligatorisk-oppgave-2/src/oppgave1/fileinfo.go
@@ -4,15 +4,15 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"bufio"
+	//"bufio"
 )
 
 func main() {
-	reader := bufio.NewReader(os.Stdin)
-	fmt.Print("Enter Filename: ")
-	text, _ := reader.ReadString('\n')
-	fmt.Println(text)
-	fi, err := os.Lstat("")
+	//reader := bufio.NewReader(os.Stdin)
+	//fmt.Print("Enter Filename: ")
+	//text, _ := reader.ReadString('\n')
+	//fmt.Println(text)
+	fi, err := os.Lstat("fileinfo.go")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -24,6 +24,12 @@ func main() {
 		fmt.Println("directory")
 	case mode&os.ModeSymlink != 0:
 		fmt.Println("symbolic link")
+	case mode&os.ModeNamedPipe != 0:
+		fmt.Println("named pipe")
+
+	case mode&os.ModeAppend != 0:
+		fmt.Println("Append")
+
 
 	}
 }

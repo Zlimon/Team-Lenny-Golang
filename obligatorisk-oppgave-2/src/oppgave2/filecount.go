9b4c0f2c71d6a66a4cd7e41cd0590fb2c0c5b166
Simon Konglevoll Lønnestad <simon.k.lonnestad@gmail.com>
2018-03-05 13:47:23 +0100

    Laget ReadFile funksjon

--- obligatorisk-oppgave-2/src/oppgave2/filecount.go
+++ obligatorisk-oppgave-2/src/oppgave2/filecount.go
@@ -1,1 +1,19 @@
+package main
 
+import (
+	"fmt"
+	"io/ioutil"
+)
+
+func main() {
+	content, err := ioutil.ReadFile("text.txt")
+	if err != nil {
+		fmt.Print(err)
+	}
+
+	fmt.Printf("File contents: %s", content)
+
+	str := string(content)
+
+	fmt.Println(str)
+}

28f6862e9f0b9d765833d27cae37bb4cb65b98a5
Petter Sølvsberg Johannessen <pettej17@uia.no>
2018-04-18 15:13:19 +0200

    Added ", client"

--- obligatorisk-oppgave-3/src/oppgave1/main.go
+++ obligatorisk-oppgave-3/src/oppgave1/main.go
@@ -8,7 +8,7 @@ import (
 func sayHello(w http.ResponseWriter, r *http.Request) {
 	message := r.URL.Path
 	message = strings.TrimPrefix(message, "/")
-	message = "Hello " + message
+	message = "Hello, client" + message
 	w.Write([]byte(message))
 }
 

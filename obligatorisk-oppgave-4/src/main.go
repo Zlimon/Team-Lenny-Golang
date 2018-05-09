be6fdcb11bc6aec7cc95ee1fc11c8448459c6c8a
Simon Konglevoll Lønnestad <simon.k.lonnestad@gmail.com>
2018-05-09 16:57:12 +0200

    GLHF

--- obligatorisk-oppgave-4/src/main.go
+++ obligatorisk-oppgave-4/src/main.go
@@ -14,18 +14,12 @@ import (
 	"html/template"
 )
 
-/*
-Setter typer fra region API.
- */
 type Pokemon struct {
 	Results	[]struct {
 		Name string `json:"name"`
 	} `json:"results"`
 }
 
-/*
-Setter typer fra Pokemon API.
- */
 type Information struct {
 	ID		int	`json:"id"`
 	Name	string `json:"name"`
@@ -75,23 +69,23 @@ type SearchBox struct {
 }
 
 var region = "kanto"
-var typeColor = "3564AE" // Standard farge
+var firstTypeColor = "3564AE" // Standard farge
 var secondTypeColor = "3564AE"
 var DittoBackground = "" // Standard bakgrunn
 var pokemonList = make([]string, 0)
 //var pokemonListKanto = make([]string, 0)
 //var pokemonListHoenn = make([]string, 0)
-//var pokemonTypeList = make([]string, 0)
+var pokemonTypeList = make([]string, 0)
 var pokemonTypeList1 string
 var pokemonTypeList2 string
-//var pokemonAbilityList = make([]string, 0)
+var pokemonAbilityList = make([]string, 0)
 var randomPokemon string
 var errorMessage string
 
 func pokemon(w http.ResponseWriter, r *http.Request) {
 	tmpl := template.Must(template.ParseFiles("C:/GitHub/Team-Lenny/obligatorisk-oppgave-4/src/pokemon.html"))
 
-	pokemon, err := http.Get("https://pokeapi.co/api/v2/pokemon/?limit=949") // Henter informasjon om Pokemon en tilfeldig Pokemon
+	pokemon, err := http.Get("https://pokeapi.co/api/v2/pokemon/?limit=949") // Henter informasjon om Pokemon
 	if err != nil {
 		fmt.Print(err.Error())
 		os.Exit(1)
@@ -105,27 +99,19 @@ func pokemon(w http.ResponseWriter, r *http.Request) {
 	var pokemonObject Pokemon
 	json.Unmarshal(pokemonData, &pokemonObject)
 
-	pokemonList := make([]string, 0)
+	pokemonList = make([]string, 0)
 	for i := 0; i < len(pokemonObject.Results); i++ {
 		pokemonList = append(pokemonList, pokemonObject.Results[i].Name)
 	}
-
-	fmt.Println(pokemonList)
 
 	searchBox := []SearchBox {
 		SearchBox{"pokemonSearch"},
 	}
 
 	rand.Seed(time.Now().UnixNano())
-	random := rand.Intn(949) // If Kanto region
+	random := rand.Intn(len(pokemonList))
 	//random := rand.Intn(len(regionObject.Pokemon)-151)+151 // If Hoenn region
 	randomPokemon = strconv.Itoa(random)
-
-	if randomPokemon == "132" {
-		DittoBackground = "http://gifimage.net/wp-content/uploads/2017/07/ditto-gif-7.gif"
-	} else {
-		DittoBackground = ""
-	}
 
 	image := "https://assets.pokemon.com/assets/cms2/img/pokedex/full/"+randomPokemon+".png"
 	searchResultINT, _ := strconv.Atoi(randomPokemon)
@@ -169,7 +155,7 @@ func pokemon(w http.ResponseWriter, r *http.Request) {
 		PageBackground: DittoBackground,
 		PokemonType1: pokemonTypeList1,
 		PokemonType2: pokemonTypeList2,
-		PokemonTypesColor: typeColor,
+		PokemonTypesColor: firstTypeColor,
 		PokemonSecondTypesColor: secondTypeColor,
 
 		PokemonID: informationObject.ID,
@@ -184,37 +170,16 @@ func pokemon(w http.ResponseWriter, r *http.Request) {
 	tmpl.Execute(w, pokemonTemplateData)
 }
 
-/*
 func kanto(w http.ResponseWriter, r *http.Request) {
 	tmpl := template.Must(template.ParseFiles("C:/GitHub/Team-Lenny/obligatorisk-oppgave-4/src/pokemon.html"))
-
-	pokemonRegion, err := http.Get("https://pokeapi.co/api/v2/pokedex/"+region+"/") // Henter informasjon om region
-	if err != nil {
-		fmt.Print(err.Error())
-		os.Exit(1)
-	}
-
-	regionData, err := ioutil.ReadAll(pokemonRegion.Body)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	var regionObject Response
-	json.Unmarshal(regionData, &regionObject)
 
 	searchBox := []SearchBox {
 		SearchBox{"pokemonSearch"},
 	}
 
 	rand.Seed(time.Now().UnixNano())
-	random := rand.Intn(len(regionObject.Pokemon)) // If Kanto region
+	random := rand.Intn(151) // If Kanto region
 	randomPokemon = strconv.Itoa(random)
-
-	if randomPokemon == "132" {
-		DittoBackground = "http://gifimage.net/wp-content/uploads/2017/07/ditto-gif-7.gif"
-	} else {
-		DittoBackground = ""
-	}
 
 	image := "https://assets.pokemon.com/assets/cms2/img/pokedex/full/"+randomPokemon+".png"
 	searchResultINT, _ := strconv.Atoi(randomPokemon)
@@ -242,15 +207,6 @@ func kanto(w http.ResponseWriter, r *http.Request) {
 	correctWeight := float64(informationObject.Weight) / 10
 	uppercaseName := strings.Title(informationObject.Name)
 
-
-	for i := 0; i < len(regionObject.Pokemon); i++ {
-		//pokemonList = append(pokemonList, strconv.Itoa(responseObject.Pokemon[i].EntryNo), "-", strings.Title(responseObject.Pokemon[i].Species.Name+", "))
-		//pokemonList = append(pokemonList, strconv.Itoa(regionObject.Pokemon[i].EntryNo))
-		pokemonListKanto = append(pokemonListKanto, strings.Title(regionObject.Pokemon[i].Species.PokemonName))
-		//fmt.Println(pokemonList[i])
-	}
-
-
 	pokemonTypeList := make([]string, 0)
 	for i := 0; i < len(informationObject.Type); i++ {
 		pokemonTypeList = append(pokemonTypeList, "[" +strings.Title(informationObject.Type[i].Type.TypeName)+ "]")
@@ -261,25 +217,26 @@ func kanto(w http.ResponseWriter, r *http.Request) {
 		pokemonAbilityList = append(pokemonAbilityList, "[" +strings.Title(informationObject.Ability[i].Ability.AbilityName)+ "]")
 	}
 
-	var pokemonData = PageData {
+	var pokemonTemplateData = PageData {
+		// Globale variabler
 		PageTitle: "Pokemon " + strings.Title(region) + " Region",
 		PageBackground: DittoBackground,
-		PokemonList: pokemonListKanto,
+		PokemonType1: pokemonTypeList1,
+		PokemonType2: pokemonTypeList2,
+		PokemonTypesColor: firstTypeColor,
+		PokemonSecondTypesColor: secondTypeColor,
+
 		PokemonID: informationObject.ID,
 		PokemonName: uppercaseName,
 		PokemonHeight: correctHeight,
 		PokemonWeight: correctWeight,
-		PokemonType1: pokemonTypeList1,
-		PokemonType2: pokemonTypeList2,
-		PokemonTypesColor: typeColor,
 		PokemonAbilities: pokemonAbilityList,
 		PokemonSearch: searchBox,
 		PokemonImage: image,
 	}
 
-	tmpl.Execute(w, pokemonData)
+	tmpl.Execute(w, pokemonTemplateData)
 }
-*/
 
 func search(w http.ResponseWriter, r *http.Request) {
 	tmpl := template.Must(template.ParseFiles("C:/GitHub/Team-Lenny/obligatorisk-oppgave-4/src/pokemon.html"))
@@ -309,21 +266,22 @@ func search(w http.ResponseWriter, r *http.Request) {
 
 	r.ParseForm()
 	searchResult := r.Form.Get("pokemonSearch")
-	pokemonAmount, _ := strconv.Atoi(searchResult)
-	searchResultID := "0"
+	lowerSearchResult := strings.ToLower(searchResult)
 
-	if pokemonAmount >= 803 {
-		errorMessage = "Du kan ikke skrive inn mer enn !" //  + strconv.Itoa(len(regionObject.Pokemon)) +
+	searchResultID := "0"
+	searchMaxAmount, _ := strconv.Atoi(lowerSearchResult)
+	if searchMaxAmount >= 803 {
+		errorMessage = "Du kan ikke skrive inn mer enn 802!" //  + strconv.Itoa(len(regionObject.Pokemon)) +
 		searchResultID = "1"
 	} else {
 		for i := range pokemonList {
-			if searchResult == pokemonList[i] {
+			if lowerSearchResult == pokemonList[i] {
 				errorMessage = ""
 				searchResultID = strconv.Itoa(i + 1)
 				break
-			} else if _, err := strconv.Atoi(searchResult); err == nil {
+			} else if _, err := strconv.Atoi(lowerSearchResult); err == nil {
 				errorMessage = ""
-				searchResultID = searchResult
+				searchResultID = lowerSearchResult
 			}
 		}
 		if searchResultID == "0" { //  || pokemonAmount > len(regionObject.Pokemon)
@@ -369,21 +327,23 @@ func search(w http.ResponseWriter, r *http.Request) {
 	}
 
 	pokemonTemplateData := PageData {
+		// Globale variabler
 		PageTitle: "Pokemon " + strings.Title(region) + " Region",
 		PageBackground: DittoBackground,
-		PokemonList: pokemonList,
+		PokemonType1: pokemonTypeList1,
+		PokemonType2: pokemonTypeList2,
+		PokemonTypesColor: firstTypeColor,
+		PokemonSecondTypesColor: secondTypeColor,
+
 		PokemonID: informationObject.ID,
 		PokemonName: uppercaseName,
 		PokemonHeight: correctHeight,
 		PokemonWeight: correctWeight,
-		PokemonType1: pokemonTypeList1,
-		PokemonType2: pokemonTypeList2,
-		PokemonTypesColor: typeColor,
-		PokemonSecondTypesColor: secondTypeColor,
 		PokemonAbilities: pokemonAbilityList,
 		PokemonSearch: searchBox,
-		SearchError: errorMessage,
 		PokemonImage: image,
+
+		SearchError: errorMessage,
 	}
 
 	tmpl.Execute(w, pokemonTemplateData)
@@ -391,7 +351,7 @@ func search(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	http.HandleFunc("/", pokemon)
-	//http.HandleFunc("/kanto", kanto)
+	http.HandleFunc("/kanto", kanto)
 	//http.HandleFunc("/selected", UserSelected)
 	http.HandleFunc("/search", search)
 	//http.HandleFunc("/hoenn", hoenn)
